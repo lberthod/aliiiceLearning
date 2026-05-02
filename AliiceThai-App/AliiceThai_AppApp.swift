23b9d68788fae8919e5b52e80041d037b98fead0
Loic Berthod <lberthod@gmail.com>
2026-05-02 10:23:09 +0700

    Initial Commit

---
+++ AliiceThai-App/AliiceThai_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AliiceThai_AppApp.swift
+//  AliiceThai-App
+//
+//  Created by Loic on 02.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct AliiceThai_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AliiceThai-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AliiceThai-App
+//
+//  Created by Loic on 02.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
